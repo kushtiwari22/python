439052c38b8158af604870fc76eeb10cb10b1caa
kushtiwari22 <152945457+kushtiwari22@users.noreply.github.com>
2024-08-18 02:16:00 +0530

    function_2.py

--- function_2.py
+++ function_2.py
@@ -1,9 +1,2 @@
-# %% [code] {"execution":{"iopub.status.busy":"2024-08-17T20:15:22.918146Z","iopub.execute_input":"2024-08-17T20:15:22.918960Z","iopub.status.idle":"2024-08-17T20:15:22.923751Z","shell.execute_reply.started":"2024-08-17T20:15:22.918922Z","shell.execute_reply":"2024-08-17T20:15:22.922279Z"}}
 def sum(a,b):
     return a+b
-    
-
-# %% [code] {"execution":{"iopub.status.busy":"2024-08-17T20:15:36.634495Z","iopub.execute_input":"2024-08-17T20:15:36.634891Z","iopub.status.idle":"2024-08-17T20:15:36.642732Z","shell.execute_reply.started":"2024-08-17T20:15:36.634861Z","shell.execute_reply":"2024-08-17T20:15:36.641415Z"}}
-
-
-# %% [code]
